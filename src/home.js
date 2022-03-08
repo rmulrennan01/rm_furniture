17b7f6661a555b0a40ae079ad60e16c002a4e0c7
rmulrennan01 <rpmulrennan@gmail.com>
2022-03-08 16:28:21 -0600

    carousel adjustments

--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import {Container} from '@material-ui/core'; 
+import {Paper} from '@material-ui/core'; 
 import Carousel from 'react-material-ui-carousel'; 
 import './Home.css';
 
@@ -8,21 +9,27 @@ import './Home.css';
 
 
 function Home() {
+    const pic = ["/product_photos/19.png", "/product_photos/untitled29.png", "/product_photos/untitled37.png", "/product_photos/20.png",
+    "/product_photos/untitled29.png", "/product_photos/untitled24.png"]; 
+    const addImages = (item) => {
+      return(
+        <Paper elevation={3}> 
+          <img className="home__carousel__image" src={item} /> 
+        </Paper>
+      );
+    }
 
 
 
-  return (
-    <div className="home">
-      Observe! My stuff!
-     
-      <Carousel className="home__carousel"  animation="slide" indicators="true" navButtonsAlwaysVisible="false" swipe="true" duration="600">
-          <img className="home__carousel__image" src="/product_photos/19.png" />
-          <img className="home__carousel__image" src="/product_photos/untitled29.png" />
-          <img className="home__carousel__image" src="/product_photos/untitled37.png" />
-          <img className="home__carousel__image" src="/product_photos/20.png" />
-          <img className="home__carousel__image" src="/product_photos/untitled29.png" />
-          <img className="home__carousel__image" src="/product_photos/untitled24.png" />
-        </Carousel>
+    return (
+      <div className="home">
+          Observe! My stuff!
+      
+          
+          <Carousel className="home__carousel"  animation="slide" indicators="true" navButtonsAlwaysVisible="false" swipe="true" duration="600">
+            {pic.map(addImages)}
+          </Carousel>
+          
         
            
         
